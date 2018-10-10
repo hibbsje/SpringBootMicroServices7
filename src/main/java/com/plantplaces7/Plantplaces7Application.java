db0def15f95a0263f21cfa09131ef47a25379f65
Administrator <Administrator@DESKTOP-099UQRG>
2018-10-09 20:06:24 -0400

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/plantplaces7/Plantplaces7Application.java
@@ -0,0 +1,12 @@
+package com.plantplaces7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plantplaces7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plantplaces7Application.class, args);
+	}
+}
