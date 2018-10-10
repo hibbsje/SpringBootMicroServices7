233fcda62257446c0c0522d4ee11e23fe3c2ca1e
Administrator <Administrator@DESKTOP-099UQRG>
2018-10-09 20:53:36 -0400

    Add javadoc, add default / endpoint

---
+++ src/main/java/com/plantplaces7/PlantPlaces7Controller.java
@@ -0,0 +1,23 @@
+package com.plantplaces7;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class PlantPlaces7Controller {
+	/** 
+	 * Handle The /start endpoint
+	 * @return
+	 */
+	@RequestMapping("/start")
+	public String start() {
+		return "start";
+	}
+	
+	@RequestMapping("/start")
+	public String index() {
+		return "start";
+	}
+
+}
